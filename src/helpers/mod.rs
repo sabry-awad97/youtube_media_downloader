966f00cdce1dfa3efbf82c979631981f6216f09b
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-03 20:02:45 +0300

    🎉 feat(Cargo.toml): Add chrono dependency with serde feature
    🚀 feat(src/helpers): Implement publish_date helper function
    
    Added the `chrono` crate with the serde feature to the project's dependencies in the `Cargo.toml` file to work with dates. This will be useful for the new feature implementation.
    
    Implemented a new helper function called `publish_date` in the `src/helpers/publish_date.rs` file. The function takes an HTML string containing a video's metadata and extracts the publish date in the format "YYYY-MM-DD" using a regular expression. It then parses the date using `chrono::NaiveDate` and returns an `Option<NaiveDate>`.
    
    Test cases were also added in the same file to ensure the correct behavior of the `publish_date` function in different scenarios:
    
    - `test_publish_date_with_valid_date`: Verifies the function's behavior with valid HTML containing a publish date.
    - `test_publish_date_with_invalid_date`: Tests the function when the HTML contains an invalid date format.
    - `test_publish_date_with_no_date`: Tests the function when there is no publish date in the HTML.
    - `test_publish_date_with_multiple_dates`: Checks that the function returns the first date found when multiple publish dates are present in the HTML.
    
    This new feature is valuable for extracting and processing video publish dates in the application.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,2 +1,5 @@
 mod regex_search;
 pub use regex_search::regex_search;
+
+mod publish_date;
+pub use publish_date::publish_date;
---
+++ src/helpers/publish_date.rs
@@ -0,0 +1,90 @@
+use super::regex_search;
+use chrono::NaiveDate;
+
+pub fn publish_date(watch_html: &str) -> Option<NaiveDate> {
+    let pattern = r#"itemprop="datePublished" content="(\d{4}-\d{2}-\d{2})""#;
+    let result = regex_search(pattern, watch_html, 1);
+    if let Ok(date_str) = result {
+        if let Ok(parsed_date) = NaiveDate::parse_from_str(date_str.as_str(), "%Y-%m-%d") {
+            return Some(parsed_date);
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_publish_date_with_valid_date() {
+        // Test with valid HTML containing a publish date
+        let html = r#"
+            <html>
+                <head>
+                    <meta itemprop="datePublished" content="2023-08-03">
+                </head>
+                <body>
+                    <p>This is a video description.</p>
+                </body>
+            </html>
+        "#;
+
+        let expected_date = NaiveDate::from_ymd_opt(2023, 8, 3);
+        assert_eq!(publish_date(html), expected_date);
+    }
+
+    #[test]
+    fn test_publish_date_with_invalid_date() {
+        // Test with HTML containing an invalid date format
+        let html = r#"
+            <html>
+                <head>
+                    <meta itemprop="datePublished" content="2023-08">
+                </head>
+                <body>
+                    <p>This is a video description.</p>
+                </body>
+            </html>
+        "#;
+
+        assert_eq!(publish_date(html), None);
+    }
+
+    #[test]
+    fn test_publish_date_with_no_date() {
+        // Test with HTML containing no publish date
+        let html = r#"
+            <html>
+                <head>
+                    <title>Test Video</title>
+                </head>
+                <body>
+                    <p>This is a video description.</p>
+                </body>
+            </html>
+        "#;
+
+        assert_eq!(publish_date(html), None);
+    }
+
+    #[test]
+    fn test_publish_date_with_multiple_dates() {
+        // Test with HTML containing multiple publish dates
+        let html = r#"
+            <html>
+                <head>
+                    <meta itemprop="datePublished" content="2023-08-03">
+                    <meta itemprop="datePublished" content="2022-12-25">
+                </head>
+                <body>
+                    <p>This is a video description.</p>
+                </body>
+            </html>
+        "#;
+
+        // The function should return the first date found
+        let expected_date = NaiveDate::from_ymd_opt(2023, 8, 3);
+        assert_eq!(publish_date(html), expected_date);
+    }
+}
