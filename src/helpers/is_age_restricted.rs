4ca8739d4eacd9f1cd92e99bbef9fc2eb44ebfbe
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-03 20:23:02 +0300

    🎉 feat(helpers): Add function to check for age-restricted content
    This commit introduces a new helper function `is_age_restricted` that checks if a given HTML content indicates age-restricted content. This function utilizes regular expression matching on the `og:restrictions:age` property in the HTML metadata.
    
    The following changes were made:
    
    - Added a new file `is_age_restricted.rs` under the `helpers` directory containing the `is_age_restricted` function.
    - Implemented tests for the `is_age_restricted` function to verify its behavior for both age-restricted and non-age-restricted scenarios.
    
    This new function will be useful for determining if certain content should be restricted based on age.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

---
+++ src/helpers/is_age_restricted.rs
@@ -0,0 +1,23 @@
+use super::regex_search;
+
+pub fn is_age_restricted(watch_html: &str) -> bool {
+    regex_search(r"og:restrictions:age", watch_html, 0).is_ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_age_restricted_true() {
+        let watch_html =
+            "<html><head><meta property=\"og:restrictions:age\" content=\"true\" /></head></html>";
+        assert!(is_age_restricted(watch_html));
+    }
+
+    #[test]
+    fn test_is_age_restricted_false() {
+        let watch_html = "<html><head></head></html>";
+        assert!(!is_age_restricted(watch_html));
+    }
+}
