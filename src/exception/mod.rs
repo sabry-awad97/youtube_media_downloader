59a020f6c833f0987cda9cd1fbc4fbbdd9e523b0
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-03 19:07:34 +0300

    ✨ feat: Add custom YouTubeError exception module
    This commit adds a new module called `YoutubeError` to handle custom exceptions related to YouTube video processing. The module defines an enum with different error variants such as `MaxRetriesExceeded`, `HTMLParseError`, `ExtractError`, `RegexMatchError`, `VideoUnavailable`, `AgeRestrictedError`, `LiveStreamError`, `VideoPrivate`, `RecordingUnavailable`, `MembersOnly`, and `VideoRegionBlocked`. Each variant represents a specific type of error that may occur during YouTube video processing.
    
    The `YoutubeError` enum implements the `fmt::Display` trait to provide human-readable error messages for each variant. This will make error handling and debugging easier by giving more descriptive information about the encountered issues.
    
    This new module will be used to handle exceptions and errors that arise during video retrieval, processing, and analysis tasks in the application.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

---
+++ src/exception/mod.rs
@@ -0,0 +1,111 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum YoutubeError {
+    MaxRetriesExceeded,
+    HTMLParseError,
+    ExtractError,
+    RegexMatchError {
+        caller: String,
+        pattern: String,
+    },
+    VideoUnavailable {
+        video_id: String,
+        error_string: String,
+    },
+    AgeRestrictedError {
+        video_id: String,
+        error_string: String,
+    },
+    LiveStreamError {
+        video_id: String,
+        error_string: String,
+    },
+    VideoPrivate {
+        video_id: String,
+        error_string: String,
+    },
+    RecordingUnavailable {
+        video_id: String,
+        error_string: String,
+    },
+    MembersOnly {
+        video_id: String,
+        error_string: String,
+    },
+    VideoRegionBlocked {
+        video_id: String,
+        error_string: String,
+    },
+}
+
+impl fmt::Display for YoutubeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            YoutubeError::MaxRetriesExceeded => write!(f, "Maximum number of retries exceeded."),
+            YoutubeError::HTMLParseError => write!(f, "HTML could not be parsed."),
+            YoutubeError::ExtractError => write!(f, "Data extraction based exception."),
+            YoutubeError::RegexMatchError { caller, pattern } => {
+                write!(f, "{}: could not find match for {}", caller, pattern)
+            }
+            YoutubeError::VideoUnavailable {
+                video_id,
+                error_string,
+            } => {
+                write!(f, "{} is unavailable: {}", video_id, error_string)
+            }
+            YoutubeError::AgeRestrictedError {
+                video_id,
+                error_string,
+            } => {
+                write!(
+                    f,
+                    "{} is age restricted, and can't be accessed without logging in: {}",
+                    video_id, error_string
+                )
+            }
+            YoutubeError::LiveStreamError {
+                video_id,
+                error_string,
+            } => {
+                write!(
+                    f,
+                    "{} is streaming live and cannot be loaded: {}",
+                    video_id, error_string
+                )
+            }
+            YoutubeError::VideoPrivate {
+                video_id,
+                error_string,
+            } => {
+                write!(f, "{} is a private video: {}", video_id, error_string)
+            }
+            YoutubeError::RecordingUnavailable {
+                video_id,
+                error_string,
+            } => {
+                write!(
+                    f,
+                    "{} does not have a live stream recording available: {}",
+                    video_id, error_string
+                )
+            }
+            YoutubeError::MembersOnly {
+                video_id,
+                error_string,
+            } => {
+                write!(f, "{} is a members-only video: {}", video_id, error_string)
+            }
+            YoutubeError::VideoRegionBlocked {
+                video_id,
+                error_string,
+            } => {
+                write!(
+                    f,
+                    "{} is not available in your region: {}",
+                    video_id, error_string
+                )
+            }
+        }
+    }
+}
