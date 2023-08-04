49571adde91847e0176ba03e59c28a38ec0c7149
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-04 06:53:32 +0300

    🎉 feat(parser): Add function to parse JSON object from HTML
    A new function `parse_for_object_from_startpoint` has been added to the `parser` module. This function is responsible for parsing a JSON object from the given HTML starting at a specific index (start_point).
    
    The function works as follows:
    1. It calls the existing `find_object_from_startpoint` function to extract the full JSON object from the HTML starting at the specified index.
    2. It then attempts to parse the extracted JSON object using `serde_json::from_str`.
    3. If the parsing is successful, the parsed JSON value is returned as `Ok(parsed)` in the `AppResult`.
    4. If the parsing fails, an `YoutubeError::HTMLParseError` is returned with a message indicating the failure.
    
    Additionally, a test case has been added in the `tests` module to validate the parsing functionality for a valid JSON object.
    
    Changes made:
    - Added `parse_for_object_from_startpoint` function to parse JSON object from HTML.
    - Implemented error handling in case of parsing failure and returning `YoutubeError::HTMLParseError`.
    - Added a test case to ensure correct parsing for a valid JSON object.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,2 +1,5 @@
 mod find_object_from_startpoint;
 pub use find_object_from_startpoint::find_object_from_startpoint;
+
+mod parse_for_object_from_startpoint;
+pub use parse_for_object_from_startpoint::parse_for_object_from_startpoint;
---
+++ src/parser/parse_for_object_from_startpoint.rs
@@ -0,0 +1,34 @@
+use super::find_object_from_startpoint;
+use crate::{AppResult, YoutubeError};
+
+pub fn parse_for_object_from_startpoint(
+    html: &str,
+    start_point: usize,
+) -> AppResult<serde_json::Value> {
+    let full_obj = find_object_from_startpoint(html, start_point)?;
+    match serde_json::from_str(&full_obj) {
+        Ok(parsed) => Ok(parsed),
+        Err(_) => Err(YoutubeError::HTMLParseError {
+            error_string: format!("Could not parse object. {}", full_obj),
+        }),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn test_valid_json_object() {
+        let html = r#"{ "name": "John", "age": 30 }"#;
+        let start_point = 0;
+        let expected_result = json!({"name": "John", "age": 30});
+
+        assert_eq!(
+            parse_for_object_from_startpoint(html, start_point),
+            Ok(expected_result)
+        );
+    }
+}
