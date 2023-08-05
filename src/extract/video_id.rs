52559d97eca564dd96c89185eb22f8270c57c353
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-05 09:04:24 +0300

    🐛 fix(extract/video_id.rs): Return AppResult in video_id function ✨ feat(extract/video_id.rs): Update tests to handle AppResult The `video_id` function in the `extract/video_id.rs` module now returns an `AppResult<String>` instead of an `Option<String>`. This allows for better error handling and propagation of errors.
    Changes made:
    - The return type of the `video_id` function is updated to `AppResult<String>`.
    - The tests for the `video_id` function have been modified to check for `Ok` and `Err` results accordingly.
    
    This change ensures that the `video_id` function can communicate errors more effectively, and the tests are adjusted to match this behavior.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/extract/video_id.rs
+++ src/extract/video_id.rs
@@ -1,11 +1,8 @@
-use crate::regex_search;
+use crate::{regex_search, AppResult};
 
-pub fn video_id(url: &str) -> Option<String> {
+pub fn video_id(url: &str) -> AppResult<String> {
     let pattern = r#"(?:v=|)([0-9A-Za-z_-]{11}).*"#;
-    match regex_search(pattern, url, 1) {
-        Ok(id) => Some(id),
-        Err(_) => None,
-    }
+    regex_search(pattern, url, 1)
 }
 
 #[cfg(test)]
@@ -15,39 +12,39 @@ mod tests {
     #[test]
     fn test_video_id_from_watch_url() {
         let url = "https://youtube.com/watch?v=dQw4w9WgXcQ";
-        assert_eq!(video_id(url), Some("dQw4w9WgXcQ".to_string()));
+        assert_eq!(video_id(url), Ok("dQw4w9WgXcQ".to_string()));
     }
 
     #[test]
     fn test_video_id_from_embed_url() {
         let url = "https://youtube.com/embed/JwYX52BP2Sk";
-        assert_eq!(video_id(url), Some("JwYX52BP2Sk".to_string()));
+        assert_eq!(video_id(url), Ok("JwYX52BP2Sk".to_string()));
     }
 
     #[test]
     fn test_video_id_from_short_url() {
         let url = "https://youtu.be/9bZkp7q19f0";
-        assert_eq!(video_id(url), Some("9bZkp7q19f0".to_string()));
+        assert_eq!(video_id(url), Ok("9bZkp7q19f0".to_string()));
     }
 
     #[test]
     fn test_video_id_invalid_url() {
         // An invalid URL that doesn't match any pattern
         let url = "https://youtube.com/random";
-        assert_eq!(video_id(url), None);
+        assert!(video_id(url).is_err());
     }
 
     #[test]
     fn test_video_id_with_query_parameters() {
         // Video id should be extracted even if there are query parameters
         let url = "https://youtube.com/watch?v=dQw4w9WgXcQ&feature=share";
-        assert_eq!(video_id(url), Some("dQw4w9WgXcQ".to_string()));
+        assert_eq!(video_id(url), Ok("dQw4w9WgXcQ".to_string()));
     }
 
     #[test]
     fn test_video_id_with_trailing_slash() {
         // Video id should be extracted even with a trailing slash
         let url = "https://youtube.com/watch?v=dQw4w9WgXcQ/";
-        assert_eq!(video_id(url), Some("dQw4w9WgXcQ".to_string()));
+        assert_eq!(video_id(url), Ok("dQw4w9WgXcQ".to_string()));
     }
 }
