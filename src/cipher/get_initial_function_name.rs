058aeffa9b9f0a6ccda9490f1021d1e728f2813d
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-05 20:09:49 +0300

    🎉 feat(cipher): Implement improved function name extraction
    The `get_initial_function_name` function has been enhanced to better extract function names from JavaScript code. This improvement allows for more accurate detection of function names, especially in cases where different patterns are used.
    
    Changes made:
    - Updated function patterns to account for variations in the function name format and added more robust matching.
    - Added test cases using the `rstest` framework to cover different scenarios and ensure the accuracy of the function name extraction.
    
    Test cases added:
    - Test with a match for a typical function pattern.
    - Test with a more complex function pattern that includes variable assignment.
    - Test with a function declared using the `function` keyword.
    - Test with a named function expression.
    
    This enhancement will provide more reliable results when analyzing JavaScript code for function names.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/cipher/get_initial_function_name.rs
+++ src/cipher/get_initial_function_name.rs
@@ -4,8 +4,8 @@ pub fn get_initial_function_name(js: &str) -> AppResult<String> {
     let function_patterns = [
         r"\b[cs]\s*&&\s*[adf]\.set\([^,]+\s*,\s*encodeURIComponent\s*\(\s*(?P<sig>[a-zA-Z0-9$]+)\(", // noqa: E501
         r"\b[a-zA-Z0-9]+\s*&&\s*[a-zA-Z0-9]+\.set\([^,]+\s*,\s*encodeURIComponent\s*\(\s*(?P<sig>[a-zA-Z0-9$]+)\(", // noqa: E501
-        r#"(?:\b|[^a-zA-Z0-9$])(?P<sig>[a-zA-Z0-9$]{2})\s*=\s*function\(\s*a\s*\)\s*{\s*a\s*=\s*a\.split\(\s*""\s*\)"#, // noqa: E501
-        r#"(?P<sig>[a-zA-Z0-9$]+)\s*=\s*function\(\s*a\s*\)\s*{\s*a\s*=\s*a\.split\(\s*""\s*\)"#, // noqa: E501
+        r#"(?:\b|[^a-zA-Z0-9$])(?P<sig>[a-zA-Z0-9$]{2})\s*=\s*function\(\s*a\s*\)\s*\{\s*a\s*=\s*a\.split\(\s*""\s*\)"#, // noqa: E501
+        r#"(?P<sig>[a-zA-Z0-9$]+)\s*=\s*function\(\s*a\s*\)\s*\{\s*a\s*=\s*a\.split\(\s*""\s*\)"#, // noqa: E501
         r#"(["\'])signature\1\s*,\s*(?P<sig>[a-zA-Z0-9$]+)\("#,
         r"\.sig\|\|(?P<sig>[a-zA-Z0-9$]+)\(",
         r"yt\.akamaized\.net/\)\s*\|\|\s*.*?\s*[cs]\s*&&\s*[adf]\.set\([^,]+\s*,\s*(?:encodeURIComponent\s*\()?\s*(?P<sig>[a-zA-Z0-9$]+)\(", // noqa: E501
@@ -33,18 +33,37 @@ pub fn get_initial_function_name(js: &str) -> AppResult<String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn test_get_initial_function_name() {
-        let js = r#"
-            function a(b) {
-                return b + 1;
-            }
-            c && adf.set("param", encodeURIComponent(a()));
-        "#;
-
-        // Test case with a match for function pattern
-        let result = get_initial_function_name(js);
-        assert_eq!(result, Ok("a".to_string()));
+    #[rstest]
+    #[case(
+        r#"c && a.set("param1", encodeURIComponent(signature($$sometext123)));"#,
+        "signature"
+    )]
+    #[case(
+        r#"var myVar = abc && xyz.set(123, encodeURIComponent(signatureFunction($)));"#,
+        "signatureFunction"
+    )]
+    #[case(
+        r#"
+            var ab = function(a) {
+                a = a.split("");
+                return a;
+            };
+        "#,
+        "ab"
+    )]
+    #[case(
+        r#"
+            var myFunction = function(a) {
+                a = a.split("");
+                return a;
+            };
+        "#,
+        "myFunction"
+    )]
+    fn test_get_initial_function_name(#[case] input: &str, #[case] expected: &str) {
+        let result = get_initial_function_name(input);
+        assert_eq!(result, Ok(expected.to_string()));
     }
 }
