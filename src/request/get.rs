959530cd8ec5d4d3384aa88918ec3f8e76d512c3
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-22 19:32:28 +0300

    🎉 feat(request): Add GET request module and tests 🐛 fix(request): Correct module import and usage A new module `get.rs` has been added under the `src/request` directory to handle HTTP GET requests. This module provides a function `get` to send GET requests with optional extra headers. Unit tests for this function have been included in the module.
    Additionally, the import and usage of the `execute_request` module have been corrected in the `mod.rs` file to ensure proper functionality.
    
    Changes made:
    - Added a new module `get.rs` with a function `get` to handle HTTP GET requests and obtain response text.
    - Added unit tests for the `get` function in the `get.rs` module.
    - Reorganized imports and module exports in the `mod.rs` file.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

---
+++ src/request/get.rs
@@ -0,0 +1,22 @@
+use super::execute_request;
+use std::collections::HashMap;
+
+pub async fn get(url: &str, extra_headers: Option<HashMap<&str, &str>>) -> reqwest::Result<String> {
+    let response = execute_request(url, reqwest::Method::GET, extra_headers, None).await?;
+    let response_text = response.text().await?;
+    Ok(response_text)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get() {
+        let url = "https://www.example.com";
+        let extra_headers = Some([("User-Agent", "Test-Agent")].iter().cloned().collect());
+        let response_text = get(url, extra_headers).await.unwrap();
+
+        assert!(!response_text.is_empty());
+    }
+}
--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,24 +1,5 @@
 mod execute_request;
+pub use execute_request::execute_request;
 
-use execute_request::execute_request;
-use std::collections::HashMap;
-
-pub async fn get(url: &str, extra_headers: Option<HashMap<&str, &str>>) -> reqwest::Result<String> {
-    let response = execute_request(url, reqwest::Method::GET, extra_headers, None).await?;
-    let response_text = response.text().await?;
-    Ok(response_text)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_get() {
-        let url = "https://www.example.com";
-        let extra_headers = Some([("User-Agent", "Test-Agent")].iter().cloned().collect());
-        let response_text = get(url, extra_headers).await.unwrap();
-
-        assert!(!response_text.is_empty());
-    }
-}
+mod get;
+pub use get::get;
